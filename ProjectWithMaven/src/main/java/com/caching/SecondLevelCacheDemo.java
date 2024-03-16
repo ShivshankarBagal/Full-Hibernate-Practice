83d96f230c695160d1c6ad44f3f739b7f233db87
shiv <shivshankarbagal1116@gmail.com>
2024-03-16 08:19:53 +0530

    second level cache

--- ProjectWithMaven/src/main/java/com/caching/SecondLevelCacheDemo.java
+++ ProjectWithMaven/src/main/java/com/caching/SecondLevelCacheDemo.java
@@ -1,8 +1,40 @@
 package com.caching;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.tut.Student;
+
+
 public class SecondLevelCacheDemo {
 	
 	public static void main(String[] args) {
+		
+		Configuration cfg=new Configuration();
+		cfg.configure();
+		SessionFactory factory=cfg.buildSessionFactory();
+		
+		//   1st session
+		Session session=factory.openSession();
+
+		Student student = session.get(Student.class, 1);
+		//here we get student data using session and then close session		
+		System.out.println(student);
+		session.close();
+		
+		
+		//2nd session
+		Session session2=factory.openSession();
+		
+		Student student2 = session2.get(Student.class, 1);
+		System.out.println(student2);
+		session2.close();
+		
+		
+		factory.close();
+		
 		
 		
 	}
--- ProjectWithMaven/src/main/java/com/tut/Student.java
+++ ProjectWithMaven/src/main/java/com/tut/Student.java
@@ -1,12 +1,18 @@
 package com.tut;
 
+import javax.persistence.Cacheable;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
 @Entity
+@Cacheable
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class Student {
 	
 	@Id
